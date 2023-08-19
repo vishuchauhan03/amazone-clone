747a4d6e489693041f11f6ea015e573b3e4e7959
vishuchauhan03 <vishalchauhan.chauhan66@gmail.com>
2023-08-19 12:42:42 +0530

    working

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import "./Checkout.css"
+import Subtotal from './Subtotal';
 function Checkout() {
   return (
-    <div>
-      
+    <div className='checkout'>
+      <div className='checkout__left'>
+        <img className='checkout__ad' src='https://www.intelligencenode.com/blog/wp-content/uploads/2019/06/Prime-day.jpg'/>
+        <div>
+          <h2 className='checkout__title'>Your Shopping Basket</h2>
+          {/* basket item */}
+          {/* basket item */}
+          {/* basket item */}
+          {/* basket item */}
+        </div>
+      </div>
+      <div className='checkout__right'>
+        <h2>the subtotal will go here</h2>
+        <Subtotal/>
+      </div>
     </div>
   )
 }
--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from "react-router-dom"
 
 function Header() {
   return (
     <div className='header'>
-      <img className='header__logo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxfYur5zlD1n-yu6wmYqqBAy1xrEDRg40WnEEfLRVVzCOMzHp60ORxtQoJPhzm_Nm1iQQ&usqp=CAU'/>
+      <Link to="/">
+        <img className='header__logo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxfYur5zlD1n-yu6wmYqqBAy1xrEDRg40WnEEfLRVVzCOMzHp60ORxtQoJPhzm_Nm1iQQ&usqp=CAU'/>
+      </Link>
       <div className='header__search'>
         <input className='header__searchInput' type='text' />
         <SearchIcon className='header__searchIcon'/>
@@ -24,10 +27,12 @@ function Header() {
             <span className='header__optionLineOne'>Your</span>
             <span className='header__optionLineTwo'>Prime</span>
         </div>
+        <Link to="/checkout">
         <div className='header__optionBasket'>
           <ShoppingBasketIcon />
           <span className='header__optionLineTwo header__basketCount'>0</span>
         </div> 
+        </Link>
       </div>
     </div>
   )
